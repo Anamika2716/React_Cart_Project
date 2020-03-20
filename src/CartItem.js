db19e768bc065e2136543247c3b12141dd70d139
Anamika <Anamika.Katyal@tatacommunications.com>
2020-03-21 00:53:55 +0530

    delete product from cart

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,67 +1,68 @@
 import React from 'react';
 
 class CartItem extends React.Component {
-    // constructor(){
-    //     super();
-    //     this.state={
-    //         price:9999,
-    //         title:"Phone",
-    //         qty:1,
-    //         img:''
-    //     }
-    //     this.testing();
-    //     //this.increaseQuantity=this.increaseQuantity.bind(this);
+    // // constructor(){
+    // //     super();
+    // //     this.state={
+    // //         price:9999,
+    // //         title:"Phone",
+    // //         qty:1,
+    // //         img:''
+    // //     }
+    // //     this.testing();
+    // //     //this.increaseQuantity=this.increaseQuantity.bind(this);
+    // // }
+    // testing(){
+    //     const promise=new Promise((resolve , reject)=>{
+    //         setTimeout(()=>{
+    //             resolve('done');
+    //
+    //         }, 5000);
+    //     })
+    //     promise.then(()=>{
+    //         //setState acts like sychronus call
+    //         this.setState({qty:this.state.qty+10});
+    //         this.setState({qty:this.state.qty+10});
+    //         this.setState({qty:this.state.qty+10});
+    //         console.log('state', this.state);
+    //     });
     // }
-    testing(){
-        const promise=new Promise((resolve , reject)=>{
-            setTimeout(()=>{
-                resolve('done');
-
-            }, 5000);
-        })
-        promise.then(()=>{
-            //setState acts like sychronus call
-            this.setState({qty:this.state.qty+10});
-            this.setState({qty:this.state.qty+10});
-            this.setState({qty:this.state.qty+10});
-            console.log('state', this.state);
-        });
-    }
-    increaseQuantity=()=>{
-        console.log("test",this.state);
-        //this.state.qty+=1;
-        //setState form 1  by giving it an object
-
-        //use when prev state is not required
-        // this.setState({
-        //         //     qty:this.state.qty+1 // merge it with state , and do shallow merging and after setState it will rerender with updated data
-        //         // })
-
-        //setState form 2 by passing function
-        //use this when prev state values are needed
-        this.setState((prevState)=>{
-            return {
-                qty: prevState.qty + 1 // again shallow merging
-            }
-        })
-
-    }
-    decreaseQuantity=()=>{
-
-        const {qty}=this.state;
-        if(qty===0)
-        {
-            return;
-        }
-        this.setState(prevState=>{
-            return {
-                qty:prevState.qty-1
-            }
-        })
-    }
+    // increaseQuantity=()=>{
+    //     console.log("test",this.state);
+    //     //this.state.qty+=1;
+    //     //setState form 1  by giving it an object
+    //
+    //     //use when prev state is not required
+    //     // this.setState({
+    //     //         //     qty:this.state.qty+1 // merge it with state , and do shallow merging and after setState it will rerender with updated data
+    //     //         // })
+    //
+    //     //setState form 2 by passing function
+    //     //use this when prev state values are needed
+    //     this.setState((prevState)=>{
+    //         return {
+    //             qty: prevState.qty + 1 // again shallow merging
+    //         }
+    //     })
+    //
+    // }
+    // decreaseQuantity=()=>{
+    //
+    //     const {qty}=this.state;
+    //     if(qty===0)
+    //     {
+    //         return;
+    //     }
+    //     this.setState(prevState=>{
+    //         return {
+    //             qty:prevState.qty-1
+    //         }
+    //     })
+    // }
     render() {
         console.log("render", this.props);
         const{price, title, qty}=this.props.product;
+        const {product, onIncreaseQuantity, onDecreaseQuantity, onDelete}=this.props;
         return (
             <div className="cart-item">
                 <div className="left-block">
@@ -76,16 +77,17 @@ class CartItem extends React.Component {
                         <img alt="increase"
                              className="action-icons"
                              src="https://image.flaticon.com/icons/svg/1665/1665578.svg"
-                             onClick={this.increaseQuantity}
+                             onClick={()=> onIncreaseQuantity(product)}
                         />
                         <img alt="decrease"
                              className="action-icons"
                              src="https://image.flaticon.com/icons/svg/1665/1665612.svg"
-                             onClick={this.decreaseQuantity}
+                             onClick={()=>onDecreaseQuantity(product)}
                         />
                         <img alt="delete"
                              className="action-icons"
                              src="https://image.flaticon.com/icons/svg/747/747370.svg"
+                             onClick={()=>onDelete(product.id)}
                         />
 
 
